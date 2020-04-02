7ebfe3f439f829721bec8883ac861411a43c305a
chetzof <vcosvic@gmail.com>
2020-04-02 17:53:30 +0300

    Added travis

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/artcucu/'
+    : '/',
   pluginOptions: {
     webpackBundleAnalyzer: {
       openAnalyzer: false,
