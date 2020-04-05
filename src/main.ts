c7dc09eaeb229c01e6d33e3c425e0a7e1f4085bc
chetzof <vcosvic@gmail.com>
2020-04-05 17:14:59 +0300

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import Vue from 'vue';
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
-import { Carousel } from 'buefy';
+import Carousel from 'buefy/src/components/carousel';
 import App from './App.vue';
 import './assets/scss/app.scss';
 import 'webp-in-css/polyfill';
 
-
 Vue.use(Carousel);
-
 Vue.config.productionTip = false;
 
 document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
