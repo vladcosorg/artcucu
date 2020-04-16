e57881cb21725b89e7e4723fcc4989039646f06c
chetzof <vcosvic@gmail.com>
2020-04-16 18:36:45 +0300

    Fix

--- src/cloudinary.ts
+++ src/cloudinary.ts
@@ -4,6 +4,7 @@ import url from 'cloudinary-core/src/url';
 const cl = new Cloudinary({
   // eslint-disable-next-line @typescript-eslint/camelcase
   cloud_name: 'dirycjknd',
+  secure: true,
 });
 
 export default cl;
