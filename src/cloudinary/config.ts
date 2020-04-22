7defa33d2942cf006818e6b147a50d6a4eeec9a3
chetzof <vcosvic@gmail.com>
2020-04-22 10:14:35 +0300

    Improved loader

--- src/cloudinary/config.ts
+++ src/cloudinary/config.ts
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 import path from 'path';
 import process from 'process';
-import { TransformationOptions } from 'cloudinary';
 
 dotenv.config({
   path: path.resolve(__dirname, '../../.env'),
--- src/cloudinary/frontend.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/camelcase */
-import { Cloudinary } from 'cloudinary-core';
-import config from '@/cloudinary/config';
-
-export default new Cloudinary({
-  // eslint-disable-next-line @typescript-eslint/camelcase
-  cloud_name: config.cloudName,
-  secure: true,
-});
